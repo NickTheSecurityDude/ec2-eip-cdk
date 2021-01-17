0ab503761e0f704a6f78de5d98a53f2fda9c2370
NickTheSecurityDude <55202293+NickTheSecurityDude@users.noreply.github.com>
2021-01-17 05:32:49 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from aws_cdk import core
 import boto3
 import sys
 
+
 client = boto3.client('sts')
 
 region=client.meta.region_name
@@ -21,16 +22,19 @@ from stacks.iam_stack import IAMStack
 from stacks.sg_stack import SGStack
 from stacks.ec2_stack import EC2Stack
 
-proj_name="linprod-mngsrv-"
+proj_name="linprod-mngsrv"
 
 app = core.App()
 
-iam_stack=IAMStack(app, proj_name+"iam",env=my_env)
-sg_stack=SGStack(app, proj_name+"sg",env=my_env)
-ec2_stack=EC2Stack(app, proj_name+"ec2",
+iam_stack=IAMStack(app, proj_name+"-iam",env=my_env)
+sg_stack=SGStack(app, proj_name+"-sg",env=my_env)
+ec2_stack=EC2Stack(app, proj_name+"-ec2",
   imaging_sg=sg_stack.imaging_sg,
   imaging_ec2_role=iam_stack.imaging_ec2_role,
   env=my_env
 )
+
+for stack in [iam_stack,sg_stack,ec2_stack]:
+  core.Tags.of(stack).add("Project", proj_name)
 
 app.synth()
